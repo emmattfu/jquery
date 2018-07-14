426fa885170e9328a9aa6070f10c74b4d8a073da
emmattfu <makcimdjan@gmail.com>
2018-07-14 16:31:28 +0300

    work is done

--- js/jquery.easyModal.js
+++ js/jquery.easyModal.js
@@ -14,35 +14,12 @@
 
     init() {
       // показываем overlay
-      // this.showOverlay();
       this.showOverlayPromise()
+           // показываем модальное окно + автоудаление если передано соответствующее свойство
           .then(this.showModalPromise())
           .catch(error => console.log(error));
-      // показываем модальное окно
-      // this.showModal();
       // Установить события
       this.setEvents();
-      // автоудаление  модального окна и оверлея
-      // this.autoCloseModal();
-    }
-
-    showOverlay() {
-      // скрываем у body боковой скролл
-      $("body").css({"overflow-y": "hidden"});
-
-      const overlay = $("<div class='overlay'></div>").css({
-        "display": "block",
-        "position": "fixed",
-        "top": 0,
-        "left": 0,
-        "width": "100%",
-        "height": "100%",
-        "z-index": 999,
-        "opacity": 0,
-        "background-color": `rgba(0, 0, 0, ${this.options.overlayOpacity})`
-      });
-
-      this.modal.before(overlay);
     }
 
     showOverlayPromise() {
@@ -104,30 +81,6 @@
       })
     }
 
-    showModal() {
-      // Получить ширину и высоту модального окна
-      const {halfWidth, halfHeight} = this.calcModalSize();
-
-      // Показать overlay
-      $(".overlay").animate({
-        "opacity": 1
-      }, this.options.duration);
-
-      // Показать модальное окно
-      this.modal.css({
-        "display": "block",
-        "position": "fixed",
-        "top": "50%",
-        "left": "50%",
-        "z-index": 1000,
-        "opacity": 0,
-        "margin-top": `-${halfHeight}px`,
-        "margin-left": `-${halfWidth}px`,
-      }).animate({
-        "opacity": 1
-      }, this.options.duration);
-    }
-
     setEvents() {
       $(".overlay").on("click", (e) => this.closeModal());
       $(this.options.closeClass).on("click", (e) => this.closeModal());
@@ -162,25 +115,7 @@
         halfHeight
       }
     }
-
-    autoCloseModal() {
-        if (this.options.autoClose) {
-            $("body").css({"overflow-y": "auto"});
-                // удаляем overlay
-                $(".overlay")
-                    .animate({"opacity": 0}, this.options.autoCloseTime, () => $(".overlay").remove());
-
-                // скрываем модальное окно
-                this.modal.animate({
-                    "opacity": 0
-                }, this.options.autoCloseTime, () => this.modal.css({"display": "none"}));
-
-                // удаляем события
-                this.clearEvents();
-        }
-    }
   }
-
 
   $.fn.easyModal = function (options) {
     new Modal(this, options).init();
