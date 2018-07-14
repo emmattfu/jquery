1ba26438149dcdf50fff01da053e77c039ed7d47
emmattfu <makcimdjan@gmail.com>
2018-07-14 16:04:31 +0300

    modal promise

--- js/jquery.easyModal.js
+++ js/jquery.easyModal.js
@@ -10,18 +10,20 @@
         autoCloseTime: 1000
       };
       this.options = $.extend(this.defaultOptions, options);
-      console.log(this.options);
     }
 
     init() {
       // показываем overlay
-      this.showOverlay();
+      // this.showOverlay();
+      this.showOverlayPromise()
+          .then(this.showModalPromise())
+          .catch(error => console.log(error));
       // показываем модальное окно
-      this.showModal();
+      // this.showModal();
       // Установить события
       this.setEvents();
       // автоудаление  модального окна и оверлея
-      this.autoCloseModal();
+      // this.autoCloseModal();
     }
 
     showOverlay() {
@@ -41,6 +43,65 @@
       });
 
       this.modal.before(overlay);
+    }
+
+    showOverlayPromise() {
+      return new Promise((resolve, reject) => {
+          $("body").css({"overflow-y": "hidden"});
+
+          const overlay = $("<div class='overlay'></div>").css({
+              "display": "block",
+              "position": "fixed",
+              "top": 0,
+              "left": 0,
+              "width": "100%",
+              "height": "100%",
+              "z-index": 999,
+              "opacity": 0,
+              "background-color": `rgba(0, 0, 0, ${this.options.overlayOpacity})`
+          });
+
+          this.modal.before(overlay);
+
+          if (overlay) {
+            resolve()
+          } else {
+            reject('No')
+          }
+      })
+    }
+
+    showModalPromise() {
+      return new Promise((resolve, reject) => {
+          // Получить ширину и высоту модального окна
+          const {halfWidth, halfHeight} = this.calcModalSize();
+
+          // Показать overlay
+          $(".overlay").animate({
+              "opacity": 1
+          }, this.options.duration);
+
+          // Показать модальное окно
+          this.modal.css({
+              "display": "block",
+              "position": "fixed",
+              "top": "50%",
+              "left": "50%",
+              "z-index": 1000,
+              "opacity": 0,
+              "margin-top": `-${halfHeight}px`,
+              "margin-left": `-${halfWidth}px`,
+          }).animate({
+              "opacity": 1
+          }, this.options.duration);
+          if (this.options.autoClose) {
+            resolve(setTimeout(() => {
+                this.closeModal();
+            }, this.options.autoCloseTime));
+          }else {
+            reject('Auto close function is disabled!')
+          }
+      })
     }
 
     showModal() {
